3a033a841778d37ea99bd959c9f80aa90f85bcd1
Kaike-stack <kaikewilliandesouza@gmail.com>
2025-03-12 21:43:09 -0300

    f

---
+++ src/main/java/criacaoDeObjetos/Main.java
@@ -0,0 +1,8 @@
+package criacaoDeObjetos;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", 2023); // Criando um objeto
+        myCar.displayInfo(); // Chama o método
+    }
+}
